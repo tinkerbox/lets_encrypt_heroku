19807c3af1cfa74da7bf0a7e7fec1043be8c88f9
Jaryl Sim <jaryl@tinkerbox.com.sg>
2016-03-27 02:11:21 +0800

    challenge engine raise record not found if challenge fails

--- app/controllers/lets_encrypt_heroku/challenges_controller.rb
+++ app/controllers/lets_encrypt_heroku/challenges_controller.rb
@@ -4,7 +4,7 @@ module LetsEncryptHeroku
     skip_before_action :require_login
 
     def show
-      @challenge = ChallengeRecord.find_by_token(params[:id])
+      @challenge = ChallengeRecord.find_by!(token: params[:id])
       render inline: @challenge.file_content, type: @challenge.content_type
     end
 
