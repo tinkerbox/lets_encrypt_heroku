6f41e2d75a0ed609ca68403b86a0084ff4ad3ac9
Jaryl Sim <jaryl@tinkerbox.com.sg>
2016-03-27 01:45:46 +0800

    move migration generator into the right module

--- lib/generators/lets_encrypt_heroku_generator.rb
+++ lib/generators/lets_encrypt_heroku_generator.rb
@@ -1,26 +1,28 @@
-require 'rails'
+require 'rails/generators/migration'
 
 module LetsEncryptHeroku
+  module Generators
 
-  class InstallGenerator < ::Rails::Generators::Base
-    include Rails::Generators::Migration
-    source_root File.expand_path('../templates', __FILE__)
+    class InstallGenerator < ::Rails::Generators::Base
+      include Rails::Generators::Migration
+      source_root File.expand_path('../templates', __FILE__)
 
-    namespace 'lets_encrypt_heroku'
+      namespace 'lets_encrypt_heroku'
 
-    def self.next_migration_number(path)
-      unless @prev_migration_nr
-        @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-      else
-        @prev_migration_nr += 1
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+       @prev_migration_nr.to_s
       end
-     @prev_migration_nr.to_s
-    end
 
-    def copy_migrations
-      migration_template 'migration.rb', 'db/migrate/create_challenge_records_table.rb'
+      def copy_migrations
+        migration_template 'migration.rb', 'db/migrate/create_challenge_records_table.rb'
+      end
+
     end
 
   end
-
 end
